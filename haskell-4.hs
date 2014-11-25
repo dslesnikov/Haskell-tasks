38100488cd86fc99df817a64e4e1bbd253987700
kreolim <suspect91717@gmail.com>
2014-11-25 13:47:02 +0500

    Refactored

--- haskell-4.hs
+++ haskell-4.hs
@@ -1,10 +1,10 @@
 data Suit = Hearts | Diamonds | Clubs | Spades
-	deriving (Show, Read, Eq)
+	deriving (Show, Eq)
 data Rank = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace
-	deriving (Show, Read, Eq, Ord)
+	deriving (Show, Eq, Ord)
 data Card = Card
-	{rank::Rank, suit::Suit }
-	deriving (Show)
+	{ rank::Rank, suit::Suit }
+	deriving (Show, Eq)
 
 court :: Card -> Bool
 court c = rank c >= Jack
@@ -19,40 +19,32 @@ beats :: Card -> Card -> Bool
 beats c1 c2 = (rank c1) > (rank c2) && (sameSuit c1 c2)
 
 beats2 :: Card -> Card -> Suit -> Bool
-beats2 (Card rank1 suit1) (Card rank2 suit2) trump = 
+beats2 c1@(Card rank1 suit1) c2@(Card rank2 suit2) trump = 
 	if suit1 == trump
 		then if suit2 == trump
 			then rank1 > rank2
 			else True
 		else if suit2 == trump
 			then False
-			else rank1 > rank2 && suit1 == suit2
+			else beats c1 c2
 
 beatsList :: [Card] -> Card -> Suit -> [Card]
 beatsList cardlist c1 trump = filter (\x -> beats2 x c1 trump) cardlist
 
 points1 :: Rank -> Int
-points1 rank = 
-	if court (Card rank Spades) && rank /= Ace
-		then 10
-		else
-			if rank == Two
-				then 2
-				else if rank == Three
-					then 3
-					else if rank == Four
-						then 4
-						else if rank == Five
-							then 5
-							else if rank == Six
-								then 6
-								else if rank == Seven
-									then 7
-									else if rank == Eight
-										then 8
-										else if rank == Nine
-											then 9
-											else 10
+points1 Two = 2
+points1 Three = 3
+points1 Four = 4
+points1 Five = 5
+points1 Six = 6
+points1 Seven = 7
+points1 Eight = 8
+points1 Nine = 9
+points1 Ten = 10
+points1 Jack = 10
+points1 Queen = 10
+points1 King = 10
+
 points :: Card -> Int
 points c = points1 $ rank c
 
