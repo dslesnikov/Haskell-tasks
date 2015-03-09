4387bbf598bf6d416fd35cf3a445e5ed99ea16d7
kreolim <suspect91717@gmail.com>
2015-03-09 15:38:15 +0500

    Haskel old task

--- haskell-5.2.hs
+++ haskell-5.2.hs
@@ -10,10 +10,10 @@ sump p1 p2 = Point (x p1 + x p2) (y p1 + y p2)
 subtrp :: Point -> Point -> Point
 subtrp p1 p2 = Point (x p1 - x p2) (y p1 - y p2)
 
-data Region =	Recktangle {vertice1::Point, vertice2::Point} |
-				Circle {center::Point, radius::Double} |
-				Union [Region] |
-				Intersection [Region]
+data Region =	Recktangle {vertice1::Point, vertice2::Point}
+				| Circle {center::Point, radius::Double}
+				| Union Region Region
+				| Intersection Region Region
 
 contains :: Region -> Point -> Bool
 contains (Recktangle p1 p2) p = 
@@ -21,8 +21,15 @@ contains (Recktangle p1 p2) p =
 	(y p1 <= y p) && (y p <= y p2)
 contains (Circle cen rad) p = 
 	(sqrt (((x cen) - (x p))*((x cen) - (x p)) + ((y cen) - (y p))*((y cen) - (y p)))) < rad
-contains (Union regs) p = 
-	any (\x -> contains x p) regs
-contains (Intersection regs) p =
-	all (\x -> contains x p) regs
+contains (Union reg1 reg2) p = 
+	contains reg1 p || contains reg2 p
+contains (Intersection reg1 reg2) p =
+	contains reg1 p && contains reg2 p
 
+isRecktangular :: Region -> Bool
+isRecktangular (Circle _ _) = False
+isRecktangular (Recktangle _ _) = True
+isRecktangular (Union (Recktangle _ _) (Recktangle _ _)) = True
+isRecktangular (Union (Circle _ _) (Circle _ _)) = False
+isRecktangular (Intersection (Recktangle _ _) (Recktangle _ _)) = True
+isRecktangular (Intersection (Circle _ _) (Circle _ _)) = False
